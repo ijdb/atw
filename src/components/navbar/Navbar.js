13b0dc41a73a56a6b8411d67f90d00a3226bf090
Isha Jain <ishajain.cv@gmail.com>
2024-06-05 14:32:24 +0530

    Creates Navbar

---
+++ src/components/navbar/Navbar.js
@@ -0,0 +1,14 @@
+import Link from "next/link";
+export default function Navbar() {
+	return (
+		<div className="flex">
+			<div>
+				<div>Atrox Technology</div>
+				<div>Communicate. Collaborate. Create</div>
+			</div>
+			<div>
+				<Link href="#"></Link>
+			</div>
+		</div>
+	);
+}
